7bb7005a25b852796926cccdf147c316eaac2b76
cesartidrick <cesartidrick@gmail.com>
2022-05-04 19:31:32 -0600

    commit tree d191851436fb3c9a937015b3dbe3351cfd6232c8

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(dogs => console.log(dogs.message[0]))
